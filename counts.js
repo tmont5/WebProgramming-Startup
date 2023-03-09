fbdc5aa425f8119b5baf8e9df573b49acfff25b8
tmont5 <montgomerytanner40@gmail.com>
2023-03-09 16:31:40 -0700

    update to update counts, now working

--- counts.js
+++ counts.js
@@ -16,6 +16,7 @@ function updateCount(itemName, mode) {
         if (likeCounts.has(itemName)) {
             console.log("Here is count before: " + likeCounts.get(itemName));
             likeCounts[itemName] = likeCounts.get(itemName) + 1;
+            likeCounts.set(itemName, likeCounts.get(itemName) + 1)
             console.log("Here is count after: " + likeCounts[itemName]);
         }
         else {
@@ -24,8 +25,12 @@ function updateCount(itemName, mode) {
     }
 
     else if (mode === 'dislike') {
+        initialize()
         if (dislikeCounts.has(itemName)) {
-
+            console.log("Here is the count: " + dislikeCounts.get(itemName));
+            dislikeCounts[itemName] = likeCounts.get(itemName) + 1;
+            dislikeCounts.set(itemName, likeCounts.get(itemName) + 1)
+            console.log("Here is count after: " + dislikeCounts[itemName])
         }
         else {
             console.log("Error: itemName does not match any ket in dislikeCounts");
